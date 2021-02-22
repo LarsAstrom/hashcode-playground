5fbb3465dd09e51233bb179cbcc18e55be4a4453
LarsAstrom <astrom.lars@telia.com>
2021-02-22 19:52:51 +0000

    scoring done

--- score.py
+++ score.py
@@ -8,5 +8,20 @@ def score(inp, out):
     ns = parse(inp)
     itr = (line for line in out.split('\n'))
     # TODO: implement
-    
-    return 0
+    D = ni(itr)
+    assert 1<=D<=ns.T2+ns.T3+ns.T4
+    assert len(out.split('\n')) == D+1
+    out = 0
+    used_pizzas = set()
+    for _ in range(D):
+        L = nl(itr)
+        assert len(L) == L[0]+1
+        L = L[1:]
+        ingreds_team = []
+        for p_id in L:
+            assert p_id not in used_pizzas
+            assert 0<=p_id<ns.M
+            used_pizzas.add(p_id)
+            ingreds_team += ns.pizzas[p_id]['ingredients']
+        out += len(set(ingreds_team))**2
+    return out
